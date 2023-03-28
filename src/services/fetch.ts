33d133f5ef012884218d241b84f35af659490ddb
Andreas Sprotte-Hansen <andreas@tunapower.dk>
2023-03-28 14:14:47 +0100

    correctly declare fetch

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,2 +1,2 @@
 import crossFetch from 'cross-fetch';
-export const fetch = (url: string, params: any) => crossFetch(url,params);
+export const fetch = (input: RequestInfo | URL, init?: RequestInit) => crossFetch(input,init);
